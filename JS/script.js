71c8d22e2a2ff0a2b66173bc7a434f29b995b9d0
sophiefau <sophiefauquembergue@gmail.com>
2024-08-22 18:02:11 +0200

    change appendChild to append

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,9 @@ function newItem(){
 
   //javascript
   //1. Adding a new item to the list of items: 
-     let li = document.createElement("li");
-     let inputValue = document.getElementById("input").value;
+     let li = $("<li></li>");
+     let li = $("<li></li>");let li = $("<li></li>");let li = $("<li></li>");
+     let inputValue = $("input").value;
      let text = document.createTextNode(inputValue);
      li.appendChild(text);
   
