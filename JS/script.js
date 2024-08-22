14c78bb17ef8a292e07eaa092d239de70d577c2c
sophiefau <sophiefauquembergue@gmail.com>
2024-08-22 18:57:10 +0200

    correct JS

--- JS/script.js
+++ JS/script.js
@@ -5,33 +5,32 @@ function newItem(){
   //1. Adding a new item to the list of items: 
      let li = $('<li></li>');
      let inputValue = $('#input').val();
-     let text = document.createTextNode(inputValue);
      li.append(inputValue);
   
      if (inputValue === '') {
        alert("You must write something!");
      } else {
-       $('#list').list.append(li);
+       $('#list').append(li);
      }
   
    //2. Crossing out an item from the list of items:
      function crossOut() {
-      li.classList.toggle("strike");
+      li.toggleClass("strike");
      }
   
-     
-     $(li).on("dblclick", function crossOut () {
-      li.classList.toggle("strike");
+     li.on("dblclick", function crossOut() {
+      li.toggleClass("strike");
     });
   
    //3(i). Adding the delete button "X": 
      let crossOutButton = $('<crossOutButton></crossOutButton>');
-     crossOutButton.append(document.createTextNode("X"));
+     crossOutButton.append(document.createTextNode('X'));
      li.append(crossOutButton);
   
-     $(crossOutButton).on("click", function deleteListItem (){
+     crossOutButton.on("click", deleteListItem); 
+     function deleteListItem (){
       li.addClass("delete");
-     });
+     }
    
    // 4. Reordering the items: 
      $('#list').sortable();
